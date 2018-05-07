7ea2cc253af7760146049a11cbc0f649db0ac2a4
ALRW <awerner1@googlemail.com>
2018-05-07 14:25:41 +0100

    [AW] completed base fwpd program

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -11,5 +11,27 @@
                   :glitter-index str->int})
 
 (defn convert
-  [vamp-key vlaue]
+  [vamp-key value]
   ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
+
+(defn mapify
+  "Return a seq of maps like {:naem \"Edwards Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+
